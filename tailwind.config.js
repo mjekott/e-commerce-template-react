2803e75fa79bd732c692ccba31e4315205cb41e3
mjekott <ekottmfon@yahoo.com>
2022-05-20 08:59:39 +0100

    build: customize theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 module.exports = {
-   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,
   theme: {
-    extend: {},
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins',sans-serif",
+        roboto: "'Roboto', sans-serif",
+      },
+      colors: {
+        primary: "#FD3057",
+      },
+    },
   },
   plugins: [],
-}
+};
